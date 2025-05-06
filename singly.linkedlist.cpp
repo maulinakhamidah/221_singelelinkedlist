79a4462d290a6a431fcaca3ac26623ed525a7862
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-06 10:40:07 +0700

    membuat struct node

---
+++ singly.linkedlist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;;
+
+// membuat struct node
+struct Node
+{
+    int noMhs;
+    Node *next;
+};
+
+// set value START = NULLL
+Node *START = NULL;
+
+// membuat prosedur addNode()
+void addNode()
+{
+    
+
+}
