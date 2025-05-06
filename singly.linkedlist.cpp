6a74e2741c9f81486a25fd207c0dc60ab1d9a327
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-06 11:41:29 +0700

    perulangan

--- singly.linkedlist.cpp
+++ singly.linkedlist.cpp
@@ -119,6 +119,17 @@ int main()
         cout << "2. Menghapus data dari dalam list" << endl;
         cout << "3. Menampilkan semua data didalam list" << endl;
         cout << "4. Mencari data dalam list" << endl;
-        cout << "5. "
+        cout << "5. Keluar" << endl;
+        cout << endl;
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+            case '1':
+            {
+                mhs.addNode();
+            }
+            break;
+        }
     }
 }
